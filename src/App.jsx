027f998de9b70628b912a0922882549310543608
Joe Haubrich <jwhaubrich@gmail.com>
2024-11-28 08:29:02 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Header from "./components/Header"
+import Entry from "./components/Entry"
+import data from "./data.js"
+
+/**
+ * Challenge:
+ * - import the array of data from data.js
+ * - map over the array to create an <Entry /> component
+ *   for every item in the data array.
+ * - display the array of Entry components in place of the current
+ *   hard-coded <Entry /> instance.
+ */
+
+
+export default function App() {
+    const journalEntries = data.map(entry => {
+        return(
+            <Entry 
+                img={entry.img}
+                title={entry.title}
+                country={entry.country}
+                googleMapsLink={entry.googleMapsLink}
+                dates={entry.dates}
+                text={entry.text}
+            />
+        )
+    }
+    )
+    
+
+    return (
+        <>
+            <Header />
+            <main className="container">
+                {journalEntries}
+            </main>
+        </>
+    )
+}
